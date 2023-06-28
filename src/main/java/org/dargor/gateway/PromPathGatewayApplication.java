44851eea3ab1c523f50a24bd23ea82499324335a
Dargor94 <franciscomasera1994@gmail.com>
2023-06-28 00:20:59 -0300

    init

---
+++ src/main/java/org/dargor/gateway/PromPathGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.dargor.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromPathGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromPathGatewayApplication.class, args);
+	}
+
+}
