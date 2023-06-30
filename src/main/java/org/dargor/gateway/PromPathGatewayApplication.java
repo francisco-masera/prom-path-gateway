fdab9fe3b8b10edd64856944b1d11ddb1f4740dd
Dargor94 <franciscomasera1994@gmail.com>
2023-06-29 23:01:37 -0300

    end

--- src/main/java/org/dargor/gateway/PromPathGatewayApplication.java
+++ src/main/java/org/dargor/gateway/PromPathGatewayApplication.java
@@ -1,13 +1,27 @@
 package org.dargor.gateway;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.http.client.HttpClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class PromPathGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PromPathGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PromPathGatewayApplication.class, args);
+    }
 
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
 }
+
