5e0a1c2c05812c46726b857ecac9e2ae7f512d99
Dargor94 <franciscomasera1994@gmail.com>
2023-07-01 20:30:21 -0300

    config

--- src/main/java/org/dargor/gateway/PromPathGatewayApplication.java
+++ src/main/java/org/dargor/gateway/PromPathGatewayApplication.java
@@ -3,15 +3,13 @@ package org.dargor.gateway;
 import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import reactor.netty.http.client.HttpClient;
 
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
-@EnableFeignClients
 public class PromPathGatewayApplication {
 
     public static void main(String[] args) {
