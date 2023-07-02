b0860906e50b3714bae8fee01c7da68bdc31570a
Dargor94 <franciscomasera1994@gmail.com>
2023-07-01 21:40:58 -0300

    gway config

--- src/main/java/org/dargor/gateway/PromPathGatewayApplication.java
+++ src/main/java/org/dargor/gateway/PromPathGatewayApplication.java
@@ -1,11 +1,8 @@
 package org.dargor.gateway;
 
-import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import reactor.netty.http.client.HttpClient;
 
 
 @EnableEurekaClient
@@ -16,10 +13,5 @@ public class PromPathGatewayApplication {
         SpringApplication.run(PromPathGatewayApplication.class, args);
     }
 
-
-    @Bean
-    public HttpClient httpClient() {
-        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
-    }
 }
 
---
+++ src/main/java/org/dargor/gateway/config/GatewayConfig.java
@@ -0,0 +1,16 @@
+package org.dargor.gateway.config;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
+
+}
