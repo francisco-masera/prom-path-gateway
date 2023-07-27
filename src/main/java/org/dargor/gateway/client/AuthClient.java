485ec6e21be93aeadde6015dbd2fa5163cae0dd7
Dargor94 <franciscomasera1994@gmail.com>
2023-07-27 18:54:21 -0300

    auth between ms

--- src/main/java/org/dargor/gateway/client/AuthClient.java
+++ src/main/java/org/dargor/gateway/client/AuthClient.java
@@ -10,14 +10,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "AuthClient",
-        url = "${routing.auth-svc.host}:${routing.auth-svc.port}/${routing.auth-svc.id}/${routing.auth-svc.url}"
+        url = "${feign.auth-svc.host}:${feign.auth-svc.port}/${feign.auth-svc.id}/${feign.auth-svc.url}"
 )
 public interface AuthClient {
 
-    @GetMapping(value = "/${routing.auth-svc.sign-up-url}", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/${feign.auth-svc.sign-up-url}", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     UserResponseDto signUp(@RequestBody SignUpRequestDto signUpRequestDto);
 
-    @PostMapping(value = "/${routing.auth-svc.login-url}", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    UserResponseDto login(@RequestBody LoginRequestDto products);
+    @PostMapping(value = "/${feign.auth-svc.login-url}", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    UserResponseDto login(@RequestBody LoginRequestDto loginRequestDto);
 
 }
